469698b606f990bcc2fa7edcbe6de5ea72240c6a
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-01-17 18:47:26 -0300

    Add SalesByDate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/header/index';
 import Filter from './components/filter/index';
+import SalesByDate from './components/sales-by-date/index';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className="app-container">
         <Filter />
+        <SalesByDate />
       </div>
     </>
   );
